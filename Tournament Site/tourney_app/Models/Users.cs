68178d035ca617abbaf265bea19992d9bcd0d2a7
mdpotter31 <mdpotter31@gmail.com>
2023-09-13 19:55:31 -0500

    Added some basic html changes and started the database.

---
+++ Tournament Site/tourney_app/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserRegistration;
+
+namespace UserRegistration
+{
+    public class Users : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+    }
+}
+
+
+public class IdentityUser : IdentityUser<string>
+{
+    public IdentityUser();
+    public IdentityUser(string userName);
+}
+
+
