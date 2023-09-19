83ddc81ee59e36355d7ee6ccd96256b6baa59f3c
mdpotter31 <mdpotter31@gmail.com>
2023-09-18 21:03:52 -0500

    Added a userlayout page for testing with some basic css.

---
+++ Tournament Site/tourney_app/Pages/UserLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace team_management_app.Pages
+{
+    public class TestingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
