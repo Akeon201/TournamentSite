c474ba9a5b48f7fa2a3eca74dcf4482d56cb8c7a
mdpotter31 <mdpotter31@gmail.com>
2023-09-19 19:15:32 -0500

    Added some refrences for buttons and new userlayout pages.

---
+++ Tournament Site/tourney_app/Pages/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace team_management_app.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
