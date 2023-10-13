4d0baca2c8b7ac2065a2344ef94124fd04a59e5a
mdpotter31 <mdpotter31@gmail.com>
2023-10-12 20:45:02 -0500

    Added a vods page for testing.

---
+++ Tournament Site/tourney_app/Pages/Vods.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace team_management_app.Pages
+{
+    public class VodsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
