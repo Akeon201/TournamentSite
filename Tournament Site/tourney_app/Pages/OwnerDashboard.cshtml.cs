f08280855ce92a9fcf2e83b0106135619ab7c748
mdpotter31 <mdpotter31@gmail.com>
2023-11-30 20:21:34 -0500

    Added a simple start to the owner dashboard page.

--- Tournament Site/tourney_app/Pages/OwnerDashboard.cshtml.cs
+++ Tournament Site/tourney_app/Pages/OwnerDashboard.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using tourney_app.Data;
 
 namespace team_management_app.Pages
 {
     public class OwnerDashboardModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        
     }
 }
